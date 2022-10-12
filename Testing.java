2e7b38943adb109457622e9670bbc3aebd0664d3
Zack3218 <114682009+Zack3218@users.noreply.github.com>
2022-10-12 18:44:29 -0500

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,5 +1,20 @@
 public class TestListing{
     public static void main (String[] args){
+        BatterListing[] batters = new BatterListing[5];
+        
+        batters[0] = new BatterListing("John", .356);
+        batters[1] = new BatterListing("Brad", .301);
+        batters[2] = new BatterListing("Steve", .289);
+        batters[3] = new BatterListing("Trayce", .439);
+        batters[4] = new BatterListing("Andrew", .358);
+        
+        for(int i = 0; i<batters.length; i++)
+            System.out.println( batters[i]);
+            
+        mergeSort(batters, 0, batters.length - 1);
+        
+        for(int i = 0; i<batters.length; i++)
+            System.out.println( batters[i]);
         
     }
     
@@ -16,7 +31,7 @@ public class TestListing{
     
     public static void merge( BatterListing[] a, int low, int mid, int high){
         BatterListing[] temp = new BatterListing[high-low+1];
-        int i = low, j = mid, n = 0;
+        int i = low, j = mid + 1, n = 0;
         
         while(i <= mid || j <= high){
             if(i>mid){
@@ -28,14 +43,18 @@ public class TestListing{
                 i++;
             }
             else if(a[i].getAverage() < a[j].getAverage()){
-                temp[n] = a[j];
-                j++;
+                temp[n] = a[i];
+                i++;
             }
             else{
                 temp[n] = a[j];
                 j++;
             }
             n++;
+        }
+        
+        for(int k = low; k <= high; k++){
+            a[k] = temp[k-low];
         }
         
     }
